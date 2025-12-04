93afad1cb1a707148ca540ac49783c1de9da93c5
Shiyan Liu <shiyanl@narval1.narval.calcul.quebec>
2025-12-04 13:53:50 -0500

    Adding logs, adding api key to client constructor.

--- const.py
+++ const.py
@@ -19,7 +19,8 @@ _llama_model = None
 def get_openai_client():
     global _client
     if _client is None:
-        _client = OpenAI()
+        api_key = os.getenv("OPENAI_API_KEY")
+        _client = OpenAI(api_key)
     return _client
 
 def get_embedding_model():
