b0f83c683042c5ed49566158b029482b2cfe2228
Shiyan <shiyan.wliu@gmail.com>
2025-12-03 22:10:05 -0500

    Experiment sampling

--- experiments/MultiTQ.py
+++ experiments/MultiTQ.py
@@ -1,37 +1,140 @@
 import json
 import argparse
+import random
 from model.agent import QueryAgent
 from model.reranker import Reranker
 from model.search import IndexSearch
 from model.tools import get_retrieval_tool, get_final_answer_tool
 from prompts.query_cot import QUERY_COT_SYSTEM_PROMPT
+from collections import defaultdict
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run the MultiTQ query agent evaluator")
 
-def main(): 
-    with open("data/MultiTQ/questions/full_questions.json", "r") as q:
+    parser.add_argument(
+        "--questions",
+        type=str,
+        required=True,
+        help="Path to the questions JSON file"
+    )
+
+    parser.add_argument(
+        "--index",
+        type=str,
+        required=True,
+        help="Path to the FAISS index file"
+    )
+
+    parser.add_argument(
+        "--metadata",
+        type=str,
+        required=True,
+        help="Path to the FAISS metadata JSON file"
+    )
+
+    parser.add_argument(
+        "--max",
+        type=int,
+        default=5,
+        help="Maximum number of questions to run"
+    )
+
+    parser.add_argument(
+        "--similarity_top_k",
+        type=int,
+        default=10,
+        help="Top-K results to retrieve via FAISS similarity search"
+    )
+
+    parser.add_argument(
+        "--rerank_top_k",
+        type=int,
+        default=50,
+        help="Top-K results to keep after reranking"
+    )
+    
+    parser.add_argument(
+        "--sample_by",
+        type=str,
+        choices=["answer_type", "time_level", "qtype", "qlabel"],
+        help="Field to group questions by before sampling"
+    )
+
+    parser.add_argument(
+        "--sample_n",
+        type=int,
+        default=None,
+        help="Number of sampled questions to run. If not provided, use all."
+    )
+
+    return parser.parse_args()
+
+def sample_questions(questions: list[dict], field: str, n: int) -> list[dict]:
+    """
+    Randomly samples questions based on specific field.
+    
+    Args:
+        questions (list[dict]): A list of question dicts.
+        field (str): The field to sample on, can be ["answer_type", "time_level", "qtype", "qlabel"].
+        n (int): The amount of questions to sample.
+        
+    Returns: 
+        samples (list[dict]): A list of the samples questions.
+    """
+    groups = defaultdict(list)
+    for q in questions:
+        key = q[field]
+        groups[key].append(q)
+
+    # randomly select a group key
+    chosen_key = random.choice(list(groups.keys()))
+    chosen_group = groups[chosen_key]
+
+    if n > len(chosen_group):
+        n = len(chosen_group)
+
+    return random.sample(chosen_group, n)
+
+def main():
+    args = parse_args()
+
+    with open(args.questions, "r") as q:
         questions = json.load(q)
 
+    if args.sample_by and args.sample_n:
+        questions = sample_questions(
+            questions,
+            field=args.sample_by,
+            n=args.sample_n
+        )
     rr = Reranker()
-    search = IndexSearch("data/tkg/MultiTQ/full/full_index.faiss", "data/tkg/MultiTQ//full/full_metadata.json")
-    
-    retreval_tool = get_retrieval_tool(search, rr, 1, 50)
+    search = IndexSearch(args.index, args.metadata)
+
+    retrieval_tool = get_retrieval_tool(
+        search=search,
+        rr=rr,
+        similarity_top_k=args.similarity_top_k,
+        rerank_top_k=args.rerank_top_k
+    )
+
     final_answer_tool = get_final_answer_tool()
-    
-    tools = [retreval_tool, final_answer_tool]
-    
+    tools = [retrieval_tool, final_answer_tool]
+
     query_agent = QueryAgent(tools, QUERY_COT_SYSTEM_PROMPT)
-    
+
     for i, question in enumerate(questions):
         query = question["question"]
         gold_answers = question["answers"]
-        qtype = question["qtype"]
-        
+
         answer = query_agent.run_agent(query)
-        
         answer_dict = query_agent.evaluate_agent_answer(answer, gold_answers)
         
         print(answer_dict)
-        if i == 3: break
+        if answer_dict["correct"]:
+            print("Correct!")
+
+        if i + 1 >= args.max:
+            break
 
 if __name__ == "__main__":
     main()
